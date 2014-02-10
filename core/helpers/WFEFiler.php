02ccc46bef8573ff14bc3429addc7ce3d8bac130
ryuzaki3698 <ryuzaki3698@hotmail.fr>
2014-02-10 05:05:22 +0100

    helpers :
    _ WFEFiler

--- core/helpers/WFEFiler.php
+++ core/helpers/WFEFiler.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace app\core\helpers;
+
 class Filer {
 
     protected static $root = APP;
--- core/helpers/WFEMailer.php
+++ core/helpers/WFEMailer.php
@@ -11,7 +11,7 @@ class Email {
     private $subject = 'subject';
     private $message = 'message';
 
-    public function __construct($to, $subject, $message, $attachment = '', $attachment_filename = '') {
+    public function __construct($to, $subject, $message) {
         $this->to = $to;
         $this->subject = $subject;
         $this->message = $message;
